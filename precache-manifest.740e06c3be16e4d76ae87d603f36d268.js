eb3b41fd2b135b0f3abdbbaa48d00e305c7df5bd
NYaremchuk <nazarmails@gmail.com>
2021-07-27 22:32:39 +0300

    Updates

---
+++ precache-manifest.740e06c3be16e4d76ae87d603f36d268.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a154cb0c56f086bcfc74203e8e566cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3c8b65641c669a9c2af6",
+    "url": "./static/css/main.0cde14ad.chunk.css"
+  },
+  {
+    "revision": "7c7a19eaf806aa4e071d",
+    "url": "./static/js/2.cad20d42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cad20d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8b65641c669a9c2af6",
+    "url": "./static/js/main.c4a95fc8.chunk.js"
+  },
+  {
+    "revision": "2a286f7172708a7e8c5a",
+    "url": "./static/js/runtime-main.3a2c9fb2.js"
+  },
+  {
+    "revision": "68065a5638cb5469149d778acf1bdf2b",
+    "url": "./static/media/Zoom_Beach_Villa.68065a56.jpg"
+  }
+]);
